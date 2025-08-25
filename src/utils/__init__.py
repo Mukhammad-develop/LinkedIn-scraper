7f63c0ee96ef1e1518cbe7764d1c5163ba48cbdb
Mukhammad-develop <115652500+Mukhammad-develop@users.noreply.github.com>
2025-08-25 02:02:53 +0100

    🔧 Fix: Add proper imports to utils __init__.py
    - Resolves import errors for utils.exceptions and utils.retry
    - Exposes key classes and functions from utils modules

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,6 @@
-# Utils Package 
+# Utils Package
+
+# Import key classes and functions for easy access
+from .exceptions import *
+from .retry import retry, exponential_backoff, CircuitBreaker, with_circuit_breaker
+from .validators import URLValidator, DataValidator, validate_scraping_parameters 
